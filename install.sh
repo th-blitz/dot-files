5c6f837fe7f7153d5950fc98cef12b2deac5004c
th-blitz <PreethamRakshith11@gmail.com>
2024-03-07 10:54:04 -0500

    changed: install.sh nvim

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-
-source public.config
-
-[ -f ~/.bashrc ] && rm ~/.bashrc && echo "[ ok ] removed ~/.bashrc"
-[ -h "$(pwd)/.bashrc" ] && unlink ~/.bashrc && echo "[ ok ] unlinked ~/.bashrc"
-ln -s $(pwd)/.bashrc ~/.bashrc && echo "[ ok ] installed ~/.bashrc"
-echo " " 
-
-[ -f ~/.gitconfig ] && rm ~/.gitconfig && echo "[ ok ] removed ~/.gitconfig"
-[ -h "$(pwd)/.gitconfig" ] && unlink ~/.gitconfig && echo "[ ok ] unlinked ~/.gitconfig"
-ln -s $(pwd)/.gitconfig ~/.gitconfig && echo "[ ok ] installed ~/.gitconfig"
-echo " "
-
+#
+#source public.config
+#
+#[ -f ~/.bashrc ] && rm ~/.bashrc && echo "[ ok ] removed ~/.bashrc"
+#[ -h "$(pwd)/.bashrc" ] && unlink ~/.bashrc && echo "[ ok ] unlinked ~/.bashrc"
+#ln -s $(pwd)/.bashrc ~/.bashrc && echo "[ ok ] installed ~/.bashrc"
+#echo " " 
+#
+#[ -f ~/.gitconfig ] && rm ~/.gitconfig && echo "[ ok ] removed ~/.gitconfig"
+#[ -h "$(pwd)/.gitconfig" ] && unlink ~/.gitconfig && echo "[ ok ] unlinked ~/.gitconfig"
+#ln -s $(pwd)/.gitconfig ~/.gitconfig && echo "[ ok ] installed ~/.gitconfig"
+#echo " "
+#
 apps=$1
 echo "[ .. ] installing .nvim" 
 cd .nvim && source ./install.sh && cd ..
 echo " "
-
-echo "[ .. ] installing .ssh" 
-cd .ssh && source ./install.sh && cd ..
-
+#
+#echo "[ .. ] installing .ssh" 
+#cd .ssh && source ./install.sh && cd ..
+#
 
